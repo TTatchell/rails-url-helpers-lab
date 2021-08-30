4e66f157d8a609d4ccd5ea61c108536bbfe6192c
TTatchell <tim.tatchell24@gmail.com>
2021-08-31 00:59:42 +1000

    upload

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,19 @@
 class Student < ActiveRecord::Base
+  after_initialize :init
+
+  def init
+    self.active = false if self.active.nil?
+  end
+
   def to_s
     self.first_name + " " + self.last_name
+  end
+
+  def active_check
+    if self.active
+      "This student is currently active."
+    else
+      "This student is currently inactive."
+    end
   end
 end
